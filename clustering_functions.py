93745a6a247cc7e7f8f7c94fbcf0d58335455a15
rbuerki <r2d4@bluewin.ch>
2019-04-19 23:43:03 +0200

    initial commit

---
+++ clustering_functions.py
@@ -0,0 +1,184 @@
+"""
+LIST OF FUNCTIONS
+-----------------
+
+
+"""
+
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_samples, silhouette_score
+
+import matplotlib.pyplot as plt
+import seaborn as sns 
+
+
+def plot_pca_results(df, pca):
+    ''' Create a DataFrame of the PCA results that includes dimension feature 
+    weights and explained variance and visusalize the results with a bar chart
+
+    Arguments:
+    ----------
+    - df: DataFrame, containing the features
+    - pca: fitted sklearn PCA class object
+
+    Returns:
+    --------
+    - None, plots PCA results (bar chart)
+    '''
+    
+    pca.fit(df)
+
+    # Index dimensions, pca components, explained variance
+    dimensions = ['Dimension {}'.format(i) for i in range(1,
+            len(pca.components_)+1)]
+    components = pd.DataFrame(np.round(pca.components_, 4), 
+            columns = list(df.keys()))
+    components.index = dimensions
+    
+    ratios = pca.explained_variance_ratio_.reshape(len(pca.components_), 1)
+    variance_ratios = pd.DataFrame(np.round(ratios, 4), 
+            columns = ['Explained Variance'])
+    variance_ratios.index = dimensions
+
+    # Create bar plot visualization: plot feature weights as function of components
+    fig, ax = plt.subplots(figsize = (16,8))
+
+    components.plot(ax = ax, kind = 'bar');
+    ax.set_ylabel("Feature Weights")
+    ax.set_xticklabels(dimensions, rotation=0)
+    plt.legend(loc='lower right')
+
+    # Display explained variance ratios
+    for i, ev in enumerate(pca.explained_variance_ratio_):
+        ax.text(i-0.40, ax.get_ylim()[1] + 0.05, "Explained Variance\n%.4f"%(ev))
+
+#     # Return concatenated DataFrame
+#     pca_results = pd.concat([variance_ratios, components], axis = 1)
+#     return pca_results
+
+
+
+def create_biplot(orig_df, reduced_df, pca, facecolors='orange'):
+    '''Create a biplot that shows a scatterplot of the reduced
+    data and the projections of the original features.
+
+    Arguments:
+    ----------
+    - orig_df: DataFrame, before pca-transformation with column names
+    - reduced_df: DataFrame, after pca-transformation (the first 2D are plotted)
+    - PCA: sklearn PCA object that contains the components_ attribute
+
+    Returns:
+    --------
+        - A matplotlib AxesSubplot object (for any additional customization)
+
+    This procedure is inspired by the script:
+    https://github.com/teddyroland/python-biplot
+    '''
+
+    fig, ax = plt.subplots(figsize = (14,8))
+
+    # Scatterplot of the reduced data    
+    ax.scatter(x=reduced_df.loc[:, 'Dimension 1'], y=reduced_df.loc[:, 'Dimension 2'], 
+        facecolors=facecolors, edgecolors='white', s=50, alpha=0.5)
+
+    # Add scaling factors to make the arrows easier to see
+    arrow_size, text_pos = 1.5, 1.5
+
+    # Add projections of the original features
+    feature_vectors = pca.components_.T
+    for i, v in enumerate(feature_vectors):
+        ax.arrow(0, 0, arrow_size*v[0], arrow_size*v[1], 
+                  head_width=0.1, head_length=0.1, linewidth=2, color='red')
+        ax.text(v[0]*text_pos, v[1]*text_pos, orig_df.columns[i], color='black', 
+                 ha='center', va='bottom', fontsize=16)
+
+    ax.set_xlabel("Dimension 1", fontsize=14)
+    ax.set_ylabel("Dimension 2", fontsize=14)
+    ax.set_title("PC plane with original feature projections", fontsize=16);
+
+    return ax
+
+
+
+    def evaluate_kmeans(df, cluster_range, pca_dim_range=[0]):
+    """Plot silhouette scores and WCSS-Inertia ('Elbow') of k-means clustering 
+    for the indicated numbers of clusters. It is possible to evaluate different 
+    PCA dimensionality reductions. (Default is None.)
+    
+    Arguments:
+    ----------
+    - df: DataFrame, containing the features
+    - pca_dim_range: iterable, containing a list of integers indicating the
+        pca_dimensions to be applied. '0' means no PCA-reduction, (Default=[0])
+    - cluster_range: iterable, containing a list of integers with the desired 
+        number of clusters
+    
+    Returns:
+    --------
+    - None, plots the Silhouette scores and WCSS-Inertia 'Ellbow' for each 
+        number of clusters for the different PCA-dimensions.
+    """ 
+    
+    df_sil_results_dict = {}
+    df_wcss_results_dict= {}
+    
+    for n_dim in tqdm(pca_dim_range):
+        if n_dim == 0:
+            df_red = df
+            label = "no PCA"
+        else: 
+            pca = PCA(n_components=n_dim)
+            df_red = pca.fit_transform(df)
+            label = str(n_dim)+"-dim"
+        
+        sil_scores_list = []
+        wcss_list = []
+        for n_clusters in cluster_range:
+            kmeans = KMeans(n_clusters=n_clusters)   
+            kmeans.fit(df_red)
+            # Predict the cluster for each data point
+            preds = kmeans.predict(df_red)
+            # Calculate mean silhouette coef for the number of clusters chosen
+            score = silhouette_score(df_red, preds)
+            # Append to list
+            sil_scores_list.append(score)
+        
+            if n_dim == 0:
+                wcss_list = list(np.zeros(len(cluster_range)))
+            if n_dim > 0:
+                wcss = kmeans.inertia_
+                wcss_list.append(wcss)
+
+        df_sil_results_dict[label] = sil_scores_list
+        df_wcss_results_dict[label] = wcss_list
+
+    sil_df = pd.DataFrame(df_sil_results_dict)
+    sil_df = sil_df.reindex(sorted(sil_df.columns), axis=1)
+    sil_df.index = cluster_range
+    
+    wcss_df = pd.DataFrame(df_wcss_results_dict)
+    wcss_df = wcss_df.reindex(sorted(wcss_df.columns), axis=1)
+    wcss_df.index = cluster_range
+    
+    plt.figure(figsize=(8,8))
+    sns.set_style("whitegrid")
+    
+    plt.subplot(2,1,1)
+    sns.lineplot(data=sil_df, palette='viridis')
+    plt.xlabel("Number of clusters")
+    plt.ylabel("Silhouette score")
+    plt.xticks(range(cluster_range[0], cluster_range[-1] + 1, 1))
+    
+    plt.subplot(2,1,2)
+    sns.lineplot(data=wcss_df, markers=True, palette='viridis')
+    plt.xlabel('Number of clusters')
+    plt.ylabel('WCSS')
+    plt.xticks(range(cluster_range[0], cluster_range[-1] + 1, 1));
+
+
