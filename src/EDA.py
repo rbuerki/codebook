65a2bfb31d848bd6bc6f816bc3385f478b6496f5
rbuerki <r2d4@bluewin.ch>
2020-11-07 00:21:20 +0100

    Addd cumsum functions

--- src/EDA.py
+++ src/EDA.py
@@ -1,4 +1,4 @@
-""" 
+"""
 LIST OF FUNCTIONS
 -----------------
 
@@ -7,7 +7,7 @@ Dataframe values:
    of distinct values for each column of the passed dataframe.
 - `display_value_counts_ptc`: Display a dataframe containing the value
    counts and their respective pct for a column or a list of columns.
-- `display_tail_transposed`: Display transposed tail of the passed 
+- `display_tail_transposed`: Display transposed tail of the passed
    dataframe with cols shown as rows and values for 5 instances as cols.
 
 Distributions:
@@ -41,9 +41,15 @@ Correlations:
 - `plot_correlations_categorical_to_target_stripplots`: Display a
   stripplot for each categorical feature in the passed dataframe to
   show the correlation to a numeric target variable.
+
+Cumulative Sums / Counts:
+- `display_cumcurve_stats`: Return a dataframe with cumsum stats for an
+  iterable of numeric values.
+- plot_cumsum_curve`: Display a cumsum curve for an iterable of numeric
+  values.
 """
 
-from typing import Optional, Tuple, Union
+from typing import Iterable, Optional, Tuple, Union
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -55,7 +61,7 @@ import seaborn as sns
 
 
 def display_distinct_values(df: Union[pd.DataFrame, pd.Series]):
-    """Display a dataframe containing the number of distinct values
+    """Return a dataframe containing the number of distinct values
     for each column of the passed dataframe.
     """
     if isinstance(df, pd.core.series.Series):
@@ -67,13 +73,13 @@ def display_distinct_values(df: Union[pd.DataFrame, pd.Series]):
         columns=["Column", "#_distinct_values"],
     )
     df_out.set_index("Column", drop=True, inplace=True)
-    display(df_out)
+    return df_out
 
 
 def display_value_counts_ptc(
     df: pd.DataFrame, n_rows: Optional[int] = None,
 ):
-    """Display a dataframe containing the value counts and their
+    """Return a dataframe containing the value counts and their
     respective pct for a column or a list of columns. The max
     number of values to display (ordered desc by counts) can be
     defined by the optional n_rows parameter.
@@ -82,19 +88,23 @@ def display_value_counts_ptc(
         df = pd.DataFrame(df)
 
     for col in df.columns:
-        counts = df[col].value_counts()
-        pct = df[col].value_counts() / len(df)
-        df_out = pd.concat([counts, pct], axis=1, keys=["counts", "pct"])
+        counts = df[col].value_counts(dropna=False)
+        prop = df[col].value_counts(dropna=False) / len(df)
+        cum_prop = np.cumsum(prop)
+        df_out = pd.concat(
+            [counts, prop, cum_prop],
+            axis=1,
+            keys=["counts", "prop", "cum_prop"],
+        )
         caption_str = f"{col}"
 
         if n_rows is not None:
             df_out = df_out.iloc[:n_rows, :]
             caption_str = "".join([caption_str, f", top {n_rows}"])
-        display(
-            df_out.style.format(
-                {"counts": "{:,.0f}", "pct": "{:.1%}"}
-            ).set_caption(caption_str)
-        )
+
+        return df_out.style.format(
+            {"counts": "{:,.0f}", "prop": "{:.1%}", "cum_prop": "{:.1%}"}
+        ).set_caption(caption_str)
 
 
 def display_tail_transposed(
@@ -108,7 +118,7 @@ def display_tail_transposed(
     df = df.sample(frac=1, random_state=random_state)
     with pd.option_context("display.max_rows", max_row):
         print(df.shape)
-        display(df.tail(5).transpose())
+        return df.tail(5).transpose()
 
 
 # DISTRIBUTIONS
@@ -190,7 +200,7 @@ def plot_categorical_pies(
     plt.show()
 
     if len(cols_with_many_distinct_values) > 0:
-        display(f"Not plotted: {cols_with_many_distinct_values}")
+        print(f"Not plotted: {cols_with_many_distinct_values}")
 
 
 # CORRELATIONS
@@ -334,9 +344,11 @@ def plot_correlations_numeric_to_target_boxplots(
         num_cols.remove(target_col)
     except ValueError:
         pass
+
+    target_col = df[target_col].astype("category")
+
     defaults = {"color": "rebeccapurple"}
     kwargs = {**defaults, **kwargs}
-
     if figsize is None:
         height = np.ceil(len(num_cols) / 2) * 3.5
         figsize = (14, height)
@@ -345,9 +357,8 @@ def plot_correlations_numeric_to_target_boxplots(
     for pos, col in enumerate(num_cols, 1):
         plt.subplot(int(np.ceil(len(num_cols) / 2)), 2, pos)
         plt.tight_layout(w_pad=1)
-        sns.boxplot(
-            data=df, x=df[target_col].astype("category"), y=col, **kwargs
-        )
+        sns.boxplot(x=target_col, y=df[col], **kwargs)
+        plt.xlabel(None)
     plt.show()
 
 
@@ -413,6 +424,7 @@ def plot_correlations_categorical_to_target_stripplots(
     plot function.
     """
     cat_cols = df.select_dtypes(include="category").columns
+
     defaults = {"palette": "rocket"}
     kwargs = {**defaults, **kwargs}
     if figsize is None:
@@ -434,3 +446,146 @@ def plot_correlations_categorical_to_target_stripplots(
 #     sns.pairplot(data=df,
 #                 x_vars=df_num.columns[i:i+5],
 #                 y_vars=['SalePrice'])
+
+
+# Cumulative COUNTS / SUMS
+
+
+def display_cumcurve_stats(
+    iterable: Iterable[Union[int, float]], threshold_list: Iterable[float]
+) -> pd.DataFrame:
+    """Return a dataframe with stats for a cumsum calculation. Input
+    variables are: an iterable of numeric values (must be all positive)
+    and an iterable containing the thresholds you want stats for
+    (values have to be in the range of (0, 1)).
+    """
+    stats_list = []
+    threshold_list_sorted = sorted(threshold_list)
+    iterable_checked = _check_iterable_for_cumsum(iterable)
+    iterable_sorted = sorted(iterable_checked, reverse=True)
+    iter_cumsum = np.cumsum(iterable_sorted)
+
+    for threshold in threshold_list_sorted:
+        thres_sum = iter_cumsum[-1] * threshold
+        n_instances = sum(iter_cumsum < thres_sum) + 1
+        prop_instances = round(n_instances / len(iter_cumsum), 2)
+        cum_value = iter_cumsum[n_instances - 1]
+
+        if n_instances != 1:
+            lowest_value = (
+                iter_cumsum[n_instances - 1] - iter_cumsum[n_instances - 2]
+            )
+        else:
+            lowest_value = iter_cumsum[n_instances - 1]
+
+        stats_list.append(
+            {
+                "value_threshold": f"{int(threshold * 100)}%",
+                "total_cum_value": cum_value,
+                "lowest_value_in_bin": lowest_value,
+                "total_cum_count": n_instances,
+                "total_cum_count_prop": prop_instances,
+            }
+        )
+    stats_df = pd.DataFrame(stats_list).set_index("value_threshold")
+    return stats_df.style.format(
+        {
+            "total_cum_value": "{:,.2f}",
+            "lowest_value_in_bin": "{:,.2f}",
+            "total_cum_count": "{:,.0f}",
+            "total_cum_count_prop": "{:.1%}",
+        }
+    )
+
+
+def plot_cumsum_curve(
+    iterable: Iterable[Union[int, float]],
+    threshold_list: Optional[Iterable[float]] = (0.2, 0.5, 0.8),
+    figsize: Optional[Tuple[int, int]] = (12, 5),
+    **kwargs,
+):
+    """Display a cumsum curve for an iterable of numeric values
+    (must be all positive). very numeric column in the passed
+    dataframe. Intercept lines are displayed for the values
+    in the optional threshold_list. (You can deactivate that
+    by setting it to None.) Optional figsize and additional
+    keyword arguments will be passed to the actual seaborn plot
+    function.
+    """
+    iterable_checked = _check_iterable_for_cumsum(iterable)
+    iterable_sorted = sorted(iterable_checked, reverse=True)
+    iter_cumsum = np.cumsum(iterable_sorted)
+    defaults = {
+        "color": "rebeccapurple",
+    }
+    kwargs = {**defaults, **kwargs}
+
+    plt.figure(figsize=figsize)
+    line = sns.lineplot(data=iter_cumsum, **kwargs)
+
+    # Make sure the xticks look good, no matter the size of the iterable
+    xtick_round_value = (len(str(int(len(iter_cumsum)))) - 2) * -1
+    xtick_interval = int(round(len(iter_cumsum) / 10, xtick_round_value))
+
+    plt.xticks(range(0, len(iter_cumsum) + 10, xtick_interval))
+    plt.ylabel("Cumulative Value", fontsize=12)
+    plt.xlabel("Count", fontsize=12)
+
+    title = "Cumulative Total Value Vs. Count of Instances"
+    if isinstance(iterable, pd.core.series.Series):
+        title = f"{iterable.name}: {title}"
+    plt.title(title, fontsize=14)
+
+    for thresh_value in [0.8, 0.2, 0.5]:
+        thresh_sum = iter_cumsum[-1] * thresh_value
+        thresh_count = sum(iter_cumsum < thresh_sum) + 1
+        y_intercept = thresh_sum
+        x_intercept = thresh_count
+        line.hlines(
+            y=thresh_sum, xmin=0, xmax=x_intercept, linewidth=0.5, color="gray"
+        )
+        line.vlines(
+            x=thresh_count,
+            ymin=0,
+            ymax=y_intercept,
+            linewidth=0.5,
+            color="gray",
+        )
+        line.annotate(
+            f"{thresh_value:.0%}",
+            xy=(-10, y_intercept + (iter_cumsum[-1] / 100)),
+            color="black",
+        )
+
+    from matplotlib.ticker import StrMethodFormatter
+
+    plt.gca().xaxis.set_major_formatter(StrMethodFormatter("{x:,.0f}"))
+    plt.gca().yaxis.set_major_formatter(StrMethodFormatter("{x:,.0f}"))
+
+    plt.show()
+
+
+def _check_iterable_for_cumsum(
+    iterable: Iterable[Union[int, float]]
+) -> Iterable[Union[int, float]]:
+    """Raise an error if the passed iterable for the cumsum functions
+    includes negative values. If there are 0 values or missing values
+    print a warning. Return the iterable without the missing values.
+    """
+    if iterable.min() < 0:
+        raise AssertionError(
+            "There cannot be negative values in the input iterable."
+        )
+    if sum(iterable == 0) > 0:
+        print(
+            f"Attention: {sum(iterable == 0)} "
+            f"instances with value 0 not included."
+        )
+    missing_values = list(filter(lambda v: v != v, iterable))
+    if len(missing_values) > 0:
+        iterable = list(filter(lambda v: v == v, iterable))
+        print(
+            f"Attention: {len(missing_values)} "
+            f"instances with missing value not included."
+        )
+    return iterable
