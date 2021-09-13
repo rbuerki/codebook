8bfdc5feca03c89c7408f8d48ff8262c388e33e4
rbuerki <r2d4@bluewin.ch>
2021-09-13 16:40:53 +0200

    Initial commit

---
+++ src/codebook/style.py
@@ -0,0 +1,50 @@
+# COLORS
+
+# 7
+green_to_grey = [
+    "#004c4c",
+    "#66b2b2",
+    "#008080",
+    "#66b2b2",
+    "#b2d8d8",
+    "#a7adba",
+    "#c0c5ce",
+]
+
+# 7
+green_to_red = [
+    "#74a09e",
+    "#86c1b2",
+    "#98e2c6",
+    "#f3c969",
+    "#f2a553",
+    "#d96548",
+    "#c14953",
+]
+
+# 10
+plasma_long = [
+    "#0d0887",
+    "#46039f",
+    "#7201a8",
+    "#9c179e",
+    "#bd3786",
+    "#d8576b",
+    "#ed7953",
+    "#fb9f3a",
+    "#fdca26",
+    "#f0f921",
+]
+
+# 6
+purple_yellow_grey_long = [
+    "rebeccapurple",
+    "#9c179e",
+    "mediumpurple",
+    "plum",
+    "#f0f921",
+    "grey",
+]
+
+# 3
+purple_yellow_grey_short = ["rebeccapurple", "yellow", "grey"]
